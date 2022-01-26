6913c06273fc843ba1f776038123a914130403a9
George Gorchev <georgegorchev@yandex.ru>
2022-01-26 23:05:05 +0300

    feat: products

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
 import '../pages/index.css'; 
+import image1 from '../images/product.jpg';
+import image2 from '../images/product2.jpg';
+import image3 from '../images/product3.jpg';
+import image4 from '../images/product4.jpg';
+
+
+console.log(document.querySelector('#img'));
+document.querySelector('#img1').src = image1;
+document.querySelector('#img2').src = image2;
+document.querySelector('#img3').src = image3;
+document.querySelector('#img4').src = image4;
+document.querySelector('#img5').src = image1;
+document.querySelector('#img6').src = image2;
+document.querySelector('#img7').src = image3;
